820228eebd04c6b9f791dd8a9f27bdb5c8265a18
JellyandChoelKi <alscjfrl824@naver.com>
2022-06-28 21:27:16 +0900

    first commit

---
+++ src/useConfirm/useConfirm.js
@@ -0,0 +1,16 @@
+export const useConfirm = (message="",onConfirm, onCancel) => {
+    if(!onConfirm || typeof callback !== "function"){
+      return;
+    }
+    if(onCancel && typeof callback !== "function"){
+      return;
+    }
+    const confirmAction = () => {
+      if(confirm(message)){
+        onConfirm();
+      }else{
+        onCancel();
+      }
+    };
+    return confirmAction;
+  }
